4ed16a5e86881f20364cb4f3a00022725959025a
IlijaRa <ilija.rakocevic@finiti.rs>
2024-06-08 14:12:25 +0200

    installed bootstrap

--- web-shop/src/router/index.js
+++ web-shop/src/router/index.js
@@ -1,21 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import LandingView from "../views/LandingView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "landing",
-      component: LandingView,
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: () => import("../views/RegisterView.vue"),
-    },
-  ],
+  routes,
 });
 
 export default router;
